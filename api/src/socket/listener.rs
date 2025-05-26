3d414168e449e6418f419b4d889fe8e97887268d
Michał Ziernik <ziernikmichal@gmail.com>
2025-05-26 07:30:34 +0200

    started working on socket io handling

--- api/src/socket/listener.rs
+++ api/src/socket/listener.rs
@@ -1,14 +1,27 @@
 use color_eyre::eyre::Result;
-use socketioxide::{SocketIo, extract::SocketRef};
+use serde::{Deserialize, Serialize};
+use socketioxide::{
+    SocketIo,
+    extract::{Data, SocketRef},
+};
+use tracing::info;
 
 use crate::state::AppState;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct JoinData {
+    code: u32,
+}
 
 pub async fn init_io(io: SocketIo, _state: AppState) -> Result<()> {
     io.ns("/", |s: SocketRef| {
         dbg!("new connection");
         // For each "message" event received, send a "message-back" event with the "Hello World!" event
-        s.on("message", |s: SocketRef| {
-            s.emit("message-back", "Hello World!").ok();
+        s.on("join", |s: SocketRef, Data(data): Data<JoinData>| {
+            info!("client connected to {}", data.code);
+        });
+        s.on_disconnect(|| {
+            dbg!("Client Disconnected");
         });
     });
 
