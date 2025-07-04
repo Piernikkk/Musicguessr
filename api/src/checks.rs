5b92ce4d329e5ae81db0698b736b97173bec1b0d
Michał Ziernik <ziernikmichal@gmail.com>
2025-07-04 17:00:13 +0200

    Api: started working message checking

--- api/src/checks.rs
+++ api/src/checks.rs
@@ -1,1 +1,48 @@
+use color_eyre::eyre::{Result, eyre};
+
+use crate::{models::Song, state::AppState};
+
+pub mod message_check;
 pub mod song_check;
+
+pub fn check_message(message: String) -> Result<()> {
+    if message.is_empty() {
+        return Err(eyre!("Message cannot be empty"));
+    }
+    if message.len() > 200 {
+        return Err(eyre!("Message is too long"));
+    }
+
+    Ok(())
+}
+
+enum CheckType {
+    Title,
+    Artist,
+    CloseTitle,
+    CloseArtist,
+}
+
+pub fn check_for_song(message: String, song: Song) -> Option<CheckType> {
+    let song_title = song.track_name.to_lowercase();
+    let song_artist = song.artist_name.to_lowercase();
+
+    let message = message.to_lowercase();
+
+    if message.contains(&song_title) {
+        return Some(CheckType::Title);
+    }
+    if message.contains(&song_artist) {
+        return Some(CheckType::Artist);
+    }
+
+    // Simple closeness check: Levenshtein distance <= 2 (requires strsim crate)
+    // if strsim::levenshtein(&message, &song_title) <= 2 {
+    //     return Some(CheckType::CloseTitle);
+    // }
+    // if strsim::levenshtein(&message, &song_artist) <= 2 {
+    //     return Some(CheckType::CloseArtist);
+    // }
+
+    None
+}
